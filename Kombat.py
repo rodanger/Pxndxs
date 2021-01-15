07923850166542d7fcae05f5efa549a8e23d9561
rodanger <71657333+rodanger@users.noreply.github.com>
2021-01-14 20:04:56 -0400

    Add files via upload

--- Kombat.py
+++ Kombat.py
@@ -2,10 +2,42 @@
 
 import pandas as pd
 
-# Declaramos la variable que nos permitira visualizar el archivo
-
-
+# Declaramos la variable indicandole que vea el archivo
+print("***************************")
 kombat= pd.read_csv('kombat1.csv')
 
 # Imprimimos el archivo
-print(kombat.head(24))
+print (kombat.head(29))
+print()
+
+#Con esto podemos visualizar los nombres de las columnas Del Dataframe
+print("***************************")
+print(kombat.columns)
+
+# Unicamente queremos ver las armas de los primeros 13 personajes
+print("***************************")
+host = pd.unique (kombat['Armas'].head(14))
+print()
+print(host)
+
+# A continuacion veremos el nivel de resistencia desde el menos resistente al mas resistente
+print("***************************")
+dif= kombat['Defensa'].describe()
+print()
+print(dif)
+print()
+# Obteniendo todas la informacion de un personaje
+print("***************************")
+print(kombat.loc[2])
+
+# Ahora vamos a acceder unicamente a 3 columnas del dataframe
+print("***************************")
+solo_3 = kombat[['Nombre ','Pais', 'Sexo']]
+print(solo_3.head(30))
+print("***************************")
+
+#Tipo de valor de cada columna
+
+tipo= kombat.dtypes
+print(tipo)
+print("***************************")
